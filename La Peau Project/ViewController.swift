e30293d85ed84ce346a4118380911e4f0a52e9a6
Sudiksha Illuri <sudiksha.illuri@gmail.com>
2022-08-04 09:36:42 -0500

    Initial Commit

---
+++ La Peau Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  La Peau Project
+//
+//  Created by Sudiksha on 8/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
